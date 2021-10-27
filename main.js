6896326f6a199951b9a5a899aec90c9eee496715
Gustavo Ribeiro <93229056+gustaribeiro@users.noreply.github.com>
2021-10-27 16:44:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+const LinksSocialMedia = {
+  github: 'gustaribeiro',
+  linkedin: 'in/gustavo-ribeiro-68421721b/',
+  instagram: 'gustavk_dev'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+    })
+}
+
+getGithubProfileInfos()
